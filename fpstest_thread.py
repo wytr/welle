bb69137ff25ae045b2efddc7272c4552364fcb7e
wytr <mwtr@tuta.io>
2022-10-15 15:27:17 +0200

    threadingexample added

---
+++ fpstest_thread.py
@@ -0,0 +1,86 @@
+from threading import Thread
+import cv2
+import datetime
+import argparse
+class FPS:
+	def __init__(self):
+		# store the start time, end time, and total number of frames
+		# that were examined between the start and end intervals
+		self._start = None
+		self._end = None
+		self._numFrames = 0
+	def start(self):
+		# start the timer
+		self._start = datetime.datetime.now()
+		return self
+	def stop(self):
+		# stop the timer
+		self._end = datetime.datetime.now()
+	def update(self):
+		# increment the total number of frames examined during the
+		# start and end intervals
+		self._numFrames += 1
+	def elapsed(self):
+		# return the total number of seconds between the start and
+		# end interval
+		return (self._end - self._start).total_seconds()
+	def fps(self):
+		# compute the (approximate) frames per second
+		return self._numFrames / self.elapsed()
+
+class WebcamVideoStream:
+	def __init__(self, src=0):
+		# initialize the video camera stream and read the first frame
+		# from the stream
+		self.stream = cv2.VideoCapture(src)
+		(self.grabbed, self.frame) = self.stream.read()
+		# initialize the variable used to indicate if the thread should
+		# be stopped
+		self.stopped = False
+	def start(self):
+		# start the thread to read frames from the video stream
+		Thread(target=self.update, args=()).start()
+		return self
+	def update(self):
+		# keep looping infinitely until the thread is stopped
+		while True:
+			# if the thread indicator variable is set, stop the thread
+			if self.stopped:
+				return
+			# otherwise, read the next frame from the stream
+			(self.grabbed, self.frame) = self.stream.read()
+	def read(self):
+		# return the frame most recently read
+		return self.frame
+	def stop(self):
+		# indicate that the thread should be stopped
+		self.stopped = True
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+print("[INFO] sampling frames from webcam...")
+stream = cv2.VideoCapture(0)
+fps = FPS().start()
+# loop over some frames
+while fps._numFrames < args["num_frames"]:
+	# grab the frame from the stream and resize it to have a maximum
+	# width of 400 pixels
+	(grabbed, frame) = stream.read()
+	# check to see if the frame should be displayed to our screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+	# update the FPS counter
+	fps.update()
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# do a bit of cleanup
+stream.release()
+cv2.destroyAllWindows()
